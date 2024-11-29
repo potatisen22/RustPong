bd352390c4898fdbca053267a9a231b40a0997ee
Vilhelm Dinevik <22potatisen@gmail.com>
2024-11-29 22:49:06 +0100

    Added ball velocity
    added velocity to ball, next is collisions!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use tetra::graphics::{self, Color, DrawParams, Texture}; //we will use textures
+use tetra::graphics::{self, Color, Rectangle, DrawParams, Texture}; //we will use textures
 use tetra::input::{self, Key}; //we will take input keys
 use tetra::math::Vec2; //will need 2d vector
+use tetra::window;
 use tetra::{Context, ContextBuilder, State}; //Will need context and state of game
 
 
@@ -8,6 +9,8 @@ const WINDOW_WIDTH: f32 = 1280.0;
 const WINDOW_HEIGHT: f32 = 720.0;
 
 const PADDLE_SPEED: f32 = 8.0;
+
+const BALL_SPEED: f32 = 5.0;
 fn main() -> tetra::Result {
     ContextBuilder::new("Pong", WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
         .quit_on_escape(true)
@@ -18,23 +21,41 @@ fn main() -> tetra::Result {
 struct Entity {
     texture: Texture,
     position: Vec2<f32>,
+    velocity: Vec2<f32>,
 }
 
 impl Entity {
     fn new(texture: Texture, position: Vec2<f32>) -> Entity {
-        Entity { texture, position }
+        Entity::with_velocity(texture, position, Vec2::zero())
+    }
+    fn with_velocity(texture: Texture, position: Vec2<f32>, velocity: Vec2<f32>) -> Entity
+    {
+        Entity
+        {
+            texture,
+            position,
+            velocity,
+        }
     }
 }
 struct GameState
 {
     player1: Entity,
     player2: Entity,
+    ball: Entity,
 }
 
 impl GameState
 {
     fn new(ctx: &mut Context) -> tetra::Result<GameState>
     {
+        let ball_velocity = Vec2::new(-BALL_SPEED, 0.0);
+        let ball_texture = Texture::new(ctx, "./resources/ballGrey.png")?;
+        let ball_position = Vec2::new(
+            WINDOW_WIDTH/2.0,
+            WINDOW_HEIGHT/2.0,
+        );
+
         let player1_texture = Texture::new(ctx, "./resources/player1.png")?;
         let player1_position = Vec2::new(
             32.0,
@@ -42,7 +63,7 @@ impl GameState
 
         let player2_texture = Texture::new(ctx, "./resources/player2.png")?;
         let player2_position = Vec2::new(
-            WINDOW_WIDTH - player2_texture.width() as f32 - 32.0,
+            WINDOW_WIDTH - 16.0,
             (WINDOW_HEIGHT - player2_texture.height() as f32) / 2.0,
         );
 
@@ -51,6 +72,7 @@ impl GameState
         Ok(GameState{
             player1: Entity::new(player1_texture, player1_position),
             player2: Entity::new(player2_texture, player2_position),
+            ball: Entity::with_velocity(ball_texture, ball_position, ball_velocity),
         })
     }
 }
@@ -58,6 +80,8 @@ impl State for GameState
 {
     fn update(&mut self, ctx: &mut Context) -> tetra::Result //update method is implemented in tetra so that it is called 60 times per second, Always.
     {
+        self.ball.position += self.ball.velocity;
+
         if input::is_key_down(ctx, Key::W)
         {
             self.player1.position.y -= PADDLE_SPEED;
@@ -83,6 +107,7 @@ impl State for GameState
         let rotation :f32= 90.0;
         self.player1.texture.draw(ctx, DrawParams::new().position(self.player1.position).rotation(rotation.to_radians()));
         self.player2.texture.draw(ctx, DrawParams::new().position(self.player2.position).rotation(rotation.to_radians()));
+        self.ball.texture.draw(ctx, self.ball.position);
 
         Ok(())
     }
